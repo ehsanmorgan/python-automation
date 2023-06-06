a357c17307329771cadcddfd24203aa66ddeeff3
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-06-07 01:33:15 +0200

    python automation

--- webdriver.py
+++ webdriver.py
@@ -1,44 +1,36 @@
-#from selenium import webdriver
-#from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.by import By
  
-# create webdriver object
-#driver = webdriver.Firefox()
- 
-# enter keyword to search
-#keyword = "real madrid"
-
-# get geeksforgeeks.org
-#driver.get("https://www.youtube.com/watch?v=eIU8uAR-TJM")
- 
-# get element
-#element = driver.find_element(By.XPATH, '//*[@id="search-input"]' )
-# print complete element
-#print(element)
-
-
 
 #from selenium import webdriver
 
 
-#url = r' /Users/fatemaabdallah/Downloads/geckodriver'
+url = r' /Users/fatemaabdallah/Downloads/geckodriver'
 
-#driver = webdriver.Firefox(executable_path = url )
+driver = webdriver.Firefox(executable_path = url )
 
-#driver.get("https://www.google.com")
+driver.get("https://www.google.com")
 
-#google_search =  driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
+google_search =  driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
 
-#google_search.send_keys("real madrid")
+google_search.send_keys("real madrid")
 
 
 
-# import webdriver
+
+
+
+
+
+
+
+# import webdriver with find element by xpath
 from selenium import webdriver
 from selenium.webdriver.common.by import By
   
 # create webdriver object
-url = r' /Users/fatemaabdallah/Downloads/geckodriver'
-driver = webdriver.Firefox(executable_path = url)
+#url = r' /Users/fatemaabdallah/Downloads/geckodriver'
+driver = webdriver.Firefox()
   
 # get geeksforgeeks.org
 driver.get("https://www.amazon.de/?&tag=hydraamazon09-21&ref=pd_sl_781ozcfkw7_e&adgrpid=71137539015&hvpone=&hvptwo=&hvadid=352566565909&hvpos=&hvnetw=g&hvrand=3627179024242247261&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9043843&hvtargid=kwd-10573980&hydadcr=12763_1720986")
